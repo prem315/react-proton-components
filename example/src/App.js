1b06cfed51b6c7b141374e65be5cf1209afb2587
Prem Patel <prem.patel3105@gmail.com>
2019-12-17 20:23:15 +0530

    init commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-import ExampleComponent from 'react-proton-components'
+import { Button } from 'react-proton-components';
 
 export default class App extends Component {
-  render () {
-    return (
-      <div>
-        <ExampleComponent text='Modern React component module' />
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div>
+				<Button />
+			</div>
+		);
+	}
 }
